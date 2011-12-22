a8ce74047ad345141d5c5f17fd25ff22eba67170
melezhik <melezhik@gmail.com>
2011-12-22 12:38:31 +0000

    Import of MELEZHIK/Bundler-v0.0.25 from CPAN.
    gitpan-cpan-distribution: Bundler
    gitpan-cpan-version:      v0.0.25
    gitpan-cpan-path:         MELEZHIK/Bundler-v0.0.25.tar.gz
    gitpan-cpan-author:       MELEZHIK
    gitpan-cpan-maturity:     released

---
+++ lib/Bundler.pm
@@ -0,0 +1,3 @@
+package Bundler;
+use version; our $VERSION = version->declare('v0.0.25');
+1;
---
+++ lib/CPANPLUS/Shell/Default/Plugins/Bundler.pm
@@ -0,0 +1,174 @@
+package CPANPLUS::Shell::Default::Plugins::Bundler;
+use CPAN::Version;
+
+### return command => method mapping
+sub plugins { ( bundle => 'bnd' ) }
+
+### method called when the command '/myplugin1' is issued
+sub bnd { 
+
+ # /helloworld bob --nofoo --bar=2 joe
+    
+ my $class   = shift;    # CPANPLUS::Shell::Default::Plugins::HW
+ my $shell   = shift;    # CPANPLUS::Shell::Default object
+ my $cb      = shift;    # CPANPLUS::Backend object
+ my $cmd     = shift;    # 'helloworld'
+ my $input   = shift;    # 'bob joe'
+ my $opts    = shift;    # { foo => 0, bar => 2 }
+        
+ $input ||= 'install';   s/\s//g for $input;
+ my $mode = $opts->{'dry-run'} ? 'dry-run' : 'real';
+ my $handler_id = $input.'::'.$mode;
+ if (exists $handlers{$handler_id}){
+   print "fire handler : $handler_id \n";
+   _bundle_file_itterator(
+      $opts->{'bundle_file'} || "$ENV{PWD}/.bundle",
+      $handlers{$handler_id},
+      $cb
+   );
+ }else{
+   print "handler [$handler_id] not found \n";
+ }
+ 
+ return;
+}
+
+
+our %handlers;
+
+$handlers{'install::dry-run'} = sub {
+	    my $line = shift;
+	    my $cb = shift;
+	    my $m = $cb->parse_module(module => $line);
+	     if ($m){
+	       if ($m->installed_version){
+	         my $action;
+	         my $st = CPAN::Version->vcmp($m->package_version,$m->installed_version);
+	          if ($st == 0){
+	            $action = 'would KEEP current version '.($m->installed_version)
+	           }elsif($st == 1){
+	            $action = "would UPDATE from version ".($m->installed_version)." to version : ".($m->package_version)
+	           }else{
+	            $action = "would DOWNGRADE from version ".($m->installed_version)." to version : ".($m->package_version);
+	           }
+	          print "$line - $action \n";
+	       }else{
+	        print "$line - would INSTALL at version : ", $m->package_version, "\n";
+	       }
+	     }else{
+	      print "[$line] - not found! \n";
+	     }
+	
+
+};
+
+$handlers{'install::real'} = sub {
+	    my $line = shift;
+	    my $cb = shift;
+	    print "do real install of module [$line]\n";
+	    print $cb->install(modules=>[$line]);
+
+};
+
+$handlers{'remove::dry-run'} = sub {
+	    my $line = shift;
+	    my $cb = shift;
+	    	my $m = $cb->parse_module(module => $line);
+	    	 if ($m){
+	    	    if ($m->installed_version){
+	              print "$line - would remove it \n";
+	            }else{
+	              print "$line - would DO*NOTHING, module is not installed \n";    
+	            }
+	        }else{
+	          print "[$line] - not found! \n";
+	       }
+
+};
+
+$handlers{'remove::real'} = sub {
+	    my $line = shift;
+	    my $cb = shift;
+	       print "do real uninstall of module [$line]\n";
+	       my $m = $cb->parse_module(module => $line);
+	       if ($m){
+	        $m->uninstall();
+	       }
+};
+
+
+### method called when the command '/? myplugin1' is issued
+sub bnd_help { 
+
+    return <<MESSAGE;
+    # Install all packages form .bundle file in current directory 
+    # or from --bundle-file option.
+    # Inspired by ruby bundler.
+    # See CPANPLUS::Shell::Default::Plugins::Bundler for details.
+
+    /bundle [install|remove] [--bundle_file path] [--dry-run]
+
+MESSAGE
+
+}
+
+
+sub _bundle_file_itterator {
+
+ my $bundle_file = shift;
+ my $handler = shift;
+ my $cb = shift;
+
+ if (-f $bundle_file){
+  print "found bundle file [$bundle_file] \n";
+    if (open BUNDLE_F, $bundle_file){
+	while (my $line = <BUNDLE_F>){
+	    chomp $line;
+	    next if $line=~/^#\s/;
+	    next if $line=~/^#/;
+	    next unless $line=~/\S/;
+	    $handler->($line,$cb);
+	}
+	close BUNDLE_F;
+    }else{
+	print "error: cannot open .bundle file [$bundle_file]: $!\n";
+    }
+ }else{
+  print "error: .bundle file [$bundle_file] not found\n";
+ }
+
+}
+
+1;
+
+__END__
+
+=head1 NAME 
+
+CPANPLUS::Shell::Default::Plugins::Bundler
+
+=head1 Author
+
+Alexey Melezhik / melezhik@gmail.com
+
+=head1 SYNOPSIS
+
+    Install all packages form .bundle file in current directory,
+    Inspired by ruby bundler.
+
+
+=head1 USAGE
+
+ 
+ # in cpanp client session
+ /bundle # installing 
+ /bundle install # installing
+ /bundle remove 
+
+=head1 OPTIONS
+
+ --bundle_file
+ --dry-run
+   
+    
+    
